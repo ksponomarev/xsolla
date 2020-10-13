c48203ddd83faafb5a2e4207822db497a75069a1
KSPonomarev <dyfo@mail.ru>
2020-10-13 18:41:44 +0300

    updates

--- files/p0/ssh_config.pp
+++ files/p0/ssh_config.pp
@@ -5,15 +5,19 @@ package { "openssh-server":
 file_line { 'PasswordAuthentication':
   ensure            => present,
   path              => '/etc/ssh/sshd_config',
-  line => 'PasswordAuthentication no',
-  match => '#PasswordAuthentication yes',
-  replace => true
+  line              => 'PasswordAuthentication no',
+  match             => '#PasswordAuthentication yes',
+  replace           => true
 }
 
 file_line { 'PubkeyAuthentication':
   ensure            => present,
   path              => '/etc/ssh/sshd_config',
-  line => 'PubkeyAuthentication yes',
-  match => '#PubkeyAuthentication yes',
-  replace => true
+  line              => 'PubkeyAuthentication yes',
+  match             => '#PubkeyAuthentication yes',
+  replace           => true
+}
+
+@@ssh_authorized_keys{"${home_dir}/.ssh/authorized_keys"
+
 }
